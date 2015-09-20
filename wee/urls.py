5601074d5c9d0e750646b1aec578363cfc2bca21
Ten-Young Guh <tenyoung795@gmail.com>
2015-09-20 05:19:53 -0400

    Spew out hotel results

--- wee/urls.py
+++ wee/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('uber_auth.urls')),
+    url(r'^app/', include('wee_app.urls')),
 ]
--- wee_app/views.py
+++ wee_app/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import requests
+from urllib.parse import quote
 
-# Create your views here.
+_PRICELINE_API_FORMAT = 'http://www.priceline.com/api/hotelretail/listing/v3/{}/{}/{}/{}/{}'
+
+def hotels(request):
+    search_term = request.GET['search-term']
+    check_in = request.GET['check-in']
+    check_out = request.GET['check-out']
+    num_rooms = request.GET['num-rooms']
+    page_size = 20
+    
+    response = requests.get(_PRICELINE_API_FORMAT.format(
+        quote(search_term), quote(check_in), quote(check_out), quote(num_rooms),
+        page_size))
+    response.raise_for_status()
+    return HttpResponse(response.text, content_type='application/json')
